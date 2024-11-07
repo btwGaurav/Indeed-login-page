78a7827299649ac51ffee7c37a26ba523b2427f7
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-11-07 11:23:18 +0530

    these are the file i made during learning java script

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// for our comparision operators
+let a = 5;
+let b = "5";
+
+console.log("a != b ", a != b );
+
+let mode = "dark";
+let color;
+
+if (mode === 
+    "light"
+    
+) {
+    color = "black"
+    
+}else {
+    color = "white"
+}
+console.log(color)
+
+let age = 21;
+
+if (age >= 21) {
+    console.log("can vote")    
+}else {
+    console.log("can not vote")
+}
+
+
+let x = 12;
+
+if (x%2 ===  0 ) {console.log(  x, "is Even")
+    
+}else(
+    console.log("=odd")
+)
